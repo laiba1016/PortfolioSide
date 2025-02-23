9c57fe1931ae2ade3b8aec684b0ef0815365832b
Laiba Nadeem <laibanadeem1026@gmail.com>
2025-02-23 11:04:21 +0500

    solve cors issue

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const dotenv = require('dotenv');
 dotenv.config()
+const cors = require('cors');
 const express = require('express');
 const connectDB = require('./database/connect');
 const contactRoutes = require('./routes/contactRoutes');
@@ -9,7 +10,6 @@ connectDB();
 
 app.use(express.json())
 app.use(cors())
-app.use('/', router)
 app.use('/api', contactRoutes)
 
 app.listen(PORT, console.log(`server is up and listening on port ${PORT}`));
