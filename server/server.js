8772f5f28cbedb52161e344d618c99f26d24a487
Laiba Nadeem <laibanadeem1026@gmail.com>
2025-02-23 13:02:36 +0500

    trying again

--- server/server.js
+++ server/server.js
@@ -9,13 +9,14 @@ const PORT = process.env.PORT
 connectDB();
 
 app.use(express.json())
-app.use(cors({
-    origin: [
-        "http://localhost:3000",
-        "https://portfolio-side-one.vercel.app"
-    ],
-    credentials: true,
-}));
+
+// app.use(cors({
+//     origin: [
+//         "http://localhost:3000",
+//         "https://portfolio-side-one.vercel.app"
+//     ],
+//     credentials: true,
+// }));
 app.use('/api', contactRoutes)
 
 app.listen(PORT, console.log(`server is up and listening on port ${PORT}`));
