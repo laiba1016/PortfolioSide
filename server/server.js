d392b7fa8a53155600eb58e2d3c6d9ed54befe53
Laiba Nadeem <laibanadeem1026@gmail.com>
2025-02-24 11:21:11 +0500

    change server file

--- server/server.js
+++ server/server.js
@@ -5,18 +5,18 @@ const express = require('express');
 const connectDB = require('./database/connect');
 const contactRoutes = require('./routes/contactRoutes');
 const app = express()
-// const PORT = process.env.PORT
+const PORT = process.env.PORT
 connectDB();
 
 app.use(express.json())
 app.use(cors({
     origin: [
-        "http://localhost:3000",
+        // "http://localhost:3000",
         "https://portfolio-side-one.vercel.app",
     ],
     credentials: true,
 }));
 app.use('/api', contactRoutes)
 
-// app.listen(PORT, console.log(`server is up and listening on port ${PORT}`));
+app.listen(PORT, console.log(`server is up and listening on port ${PORT}`));
 module.exports = app;
